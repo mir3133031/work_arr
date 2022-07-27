52dc835522fe85b3b738df6d2cf2fd38803ff0ee
мир <106350647+mir3133031@users.noreply.github.com>
2022-07-27 14:50:37 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,59 @@
+// let car = [
+//     {
+//         name: 'malibu',
+//         price: 24000
+//     },
+//     {
+//         name: 'Matiz',
+//         price: 3500
+//     },
+//     {
+//         name: 'tahoe',
+//         price: 80000
+//     },
+//     {
+//         name: 'Nexia',
+//         price: 10000
+//     },
+//     {
+//         name: 'Gentra',
+//         price: 15000
+//     },
+//     {
+//         name: 'tracker',
+//         price: 22000
+//     }
+// ]
+// let cheap = []
+// car.filter(item => {
+//     if (item.price >= 35000) cheap.push(item);
+
+
+// })
+
+
+
+
+// let arr = ['Aleksey', 'Kartoshka', 'Margarita', 'Morgenshtern', 'Monica']
+// let index = prompt()
+// arr.splice(index, 1)
+// if (index >= 5) alert(`не найдено`)
+// console.log(arr);
+
+
+
+
+
+
+// let arr_tw = [1, 2, false, 'hello', 24, 'world', undefined, null, 'error', 22]
+// let num = []
+// arr_tw.filter(item => {
+//     if (typeof (item) === `number`) num.push(item)
+
+// })
+// if (num.length >= 5) {
+//     alert(`good`)
+// } else {
+//     alert(`bad`)
+// }
+
